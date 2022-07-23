ffed6f005bc0e8d9a48893f1a8083732047c8bff
Reaper <ahoy@barelyhuman.dev>
2022-07-23 18:28:35 +0530

    fix path for the db

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,23 @@
+const path = require("path");
 // Update with your config settings.
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
+
+const dbPath = path.resolve(path.join(process.cwd(), "db", "db.sqlite3"));
+
 module.exports = {
   development: {
-    client: 'better-sqlite3',
+    client: "better-sqlite3",
     connection: {
-      filename: './db/db.sqlite3',
+      filename: dbPath,
     },
   },
   production: {
-    client: 'better-sqlite3',
+    client: "better-sqlite3",
     connection: {
-      filename: './db/db.sqlite3',
+      filename: dbPath,
     },
   },
 };
