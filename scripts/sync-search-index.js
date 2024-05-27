a3d62a7c0b77c45373009e94a6e40d698f95e478
Reaper Gelera <ahoy@barelyhuman.dev>
2024-05-27 10:05:51 +0530

    chore: cleanup

--- scripts/sync-search-index.js
+++ scripts/sync-search-index.js
@@ -1,26 +1,32 @@
-const {conch} = require("@barelyreaper/conch");
-const { db } = require("../db/db");
+const { conch } = require('@barelyreaper/conch');
+const { db } = require('../db/db');
 
 async function main() {
-  await db("roms_search_index").del();
-  const romData = await db("roms_devices_mapping")
-    .leftJoin("roms", "roms_devices_mapping.rom_id", "roms.id")
-    .leftJoin("devices", "roms_devices_mapping.device_id", "devices.id")
+  await db('roms_search_index').del();
+  const romData = await db('roms_devices_mapping')
+    .leftJoin('roms', 'roms_devices_mapping.rom_id', 'roms.id')
+    .leftJoin('devices', 'roms_devices_mapping.device_id', 'devices.id')
     .select(
-      "roms_devices_mapping.*",
-      "roms.name as rom_name",
-      "devices.basename as device_name",
-      "devices.codename as device_codename",
+      'roms_devices_mapping.*',
+      'roms.name as rom_name',
+      'devices.basename as device_name',
+      'devices.codename as device_codename'
     );
 
-  await conch(romData, async (row) => {
-    const execChain = await db("roms_search_index").insert({
-      rom_mapping_id: row.id,
-      keywords: [row.rom_name, row.device_name, row.device_codename].join(", "),
-    });
-  }, { limit: 20 });
+  await conch(
+    romData,
+    async row => {
+      await db('roms_search_index').insert({
+        rom_mapping_id: row.id,
+        keywords: [row.rom_name, row.device_name, row.device_codename].join(
+          ', '
+        ),
+      });
+    },
+    { limit: 20 }
+  );
 }
 
-exports.syncSearchIndex = main
+exports.syncSearchIndex = main;
 
 if (require.main === module) main().then(() => process.exit(0));
