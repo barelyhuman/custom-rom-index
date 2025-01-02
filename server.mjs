86888f1441fbb28da668a54d09197085df15240b
reaper <ahoy@barelyhuman.dev>
2025-01-02 16:26:08 +0530

    chore: add fly deploy

--- server.mjs
+++ server.mjs
@@ -1,26 +1,26 @@
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import fastifyStatic from '@fastify/static'
-import fastify from 'fastify'
-import { h } from 'preact'
-import { renderToString } from 'preact-render-to-string'
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import fastifyStatic from "@fastify/static";
+import fastify from "fastify";
+import { h } from "preact";
+import { renderToString } from "preact-render-to-string";
 
-const app = fastify({ logger: true })
+const app = fastify({ logger: true });
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.register(fastifyStatic, {
-  root: path.join(__dirname, 'out/client'),
-  prefix: '/client',
-})
+  root: path.join(__dirname, "out/client"),
+  prefix: "/client",
+});
 
 async function mapComponent(path, req, res) {
-  const appFile = await import('./out/_app.js')
-  const Component = await import(path)
+  const appFile = await import("./out/_app.js");
+  const Component = await import(path);
   let pageProps = {
     props: {},
-  }
-  if ('getServerSideProps' in Component) {
+  };
+  if ("getServerSideProps" in Component) {
     Object.assign(
       pageProps,
       await Component.getServerSideProps({
@@ -28,16 +28,16 @@ async function mapComponent(path, req, res) {
         response: res,
         query: req.query,
       })
-    )
+    );
   }
-  const mod = 'default' in appFile ? appFile.default : appFile
+  const mod = "default" in appFile ? appFile.default : appFile;
   const FinalComponent = () =>
     h(mod, {
       Component: Component.default,
       pageProps: pageProps.props,
-    })
-  const toString = renderToString(h(FinalComponent))
-  console.log(toString)
+    });
+  const toString = renderToString(h(FinalComponent));
+  console.log(toString);
   return toString
     .replace(
       /<\/head\>/,
@@ -45,25 +45,30 @@ async function mapComponent(path, req, res) {
     )
     .replace(
       /<\/body\>/,
-      `<script type="module" src="/client/${path.replace('./out/', '')}"></script>
-    <script type="application/json" id="pageProps">${JSON.stringify(pageProps.props)}</script>
+      `<script type="module" src="/client/${path.replace(
+        "./out/",
+        ""
+      )}"></script>
+    <script type="application/json" id="pageProps">${JSON.stringify(
+      pageProps.props
+    )}</script>
 </body>`
-    )
+    );
 }
 
-app.get('/', async (req, res) => {
-  res.header('content-type', 'text/html')
-  return mapComponent('./out/index.js', req, res)
-})
+app.get("/", async (req, res) => {
+  res.header("content-type", "text/html");
+  return mapComponent("./out/index.js", req, res);
+});
 
-app.get('/devices', async (req, res) => {
-  res.header('content-type', 'text/html')
-  return mapComponent('./out/devices.js', req, res)
-})
+app.get("/devices", async (req, res) => {
+  res.header("content-type", "text/html");
+  return mapComponent("./out/devices.js", req, res);
+});
 
-app.get('/submit-rom', async (req, res) => {
-  res.header('content-type', 'text/html')
-  return mapComponent('./out/submit-rom.js', req, res)
-})
+app.get("/submit-rom", async (req, res) => {
+  res.header("content-type", "text/html");
+  return mapComponent("./out/submit-rom.js", req, res);
+});
 
-await app.listen({ port: 3000 })
+await app.listen({ host: "0.0.0.0", port: 3000 });
