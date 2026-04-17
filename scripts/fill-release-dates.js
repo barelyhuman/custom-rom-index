0d05c6583af91806f0e87eff2fc27250b0c349e1
reaper <ahoy@barelyhuman.dev>
2026-04-17 20:52:14 +0530

    chore: update device dates

--- scripts/fill-release-dates.js
+++ scripts/fill-release-dates.js
@@ -1,73 +1,75 @@
 #!/usr/bin/env node
 
-import got from 'got'
-import kluer from 'kleur'
-import YAML from 'yaml'
-const info = kluer.cyan().bold
-import { fileURLToPath } from 'url'
-import { conch } from '@barelyreaper/conch'
-import { db } from '../db/db.js'
-import { getReleasedOn } from '../lib/date-utils.js'
+import got from "got";
+import kluer from "kleur";
+import YAML from "yaml";
+const info = kluer.cyan().bold;
+import { fileURLToPath } from "url";
+import { conch } from "@barelyreaper/conch";
+import { db } from "../db/db.js";
+import { getReleasedOn } from "../lib/date-utils.js";
 
-const URL_TEMPLATE = deviceCodeName =>
-  `https://raw.githubusercontent.com/PixelExperience/wiki/master/_data/devices/${deviceCodeName}.yml`
+const URL_TEMPLATE = (deviceCodeName) =>
+  `https://raw.githubusercontent.com/PixelExperience/wiki/main/_data/devices/${deviceCodeName}.yml`;
 
-const URL_TEMPLATE_TWO = deviceCodeName =>
-  `https://raw.githubusercontent.com/LineageOS/lineage_wiki/master/_data/devices/${deviceCodeName}.yml`
+const URL_TEMPLATE_TWO = (deviceCodeName) =>
+  `https://raw.githubusercontent.com/LineageOS/lineage_wiki/main/_data/devices/${deviceCodeName}.yml`;
 
 function parseYAML(text) {
   try {
-    return text && YAML.parse(text)
+    return text && YAML.parse(text);
   } catch (err) {
-    return null
+    return null;
   }
 }
 
 async function deviceInfoAPI(codename) {
   try {
-    const response = await got(URL_TEMPLATE(codename)).catch(err => err)
-    const responseTwo = await got(URL_TEMPLATE_TWO(codename)).catch(err => err)
-    const text = response.body
-    const textTwo = responseTwo.body
-    const parsed = parseYAML(text)
-    const parsedTwo = parseYAML(textTwo)
+    const response = await got(URL_TEMPLATE(codename)).catch((err) => err);
+    const responseTwo = await got(URL_TEMPLATE_TWO(codename)).catch(
+      (err) => err,
+    );
+    const text = response.body;
+    const textTwo = responseTwo.body;
+    const parsed = parseYAML(text);
+    const parsedTwo = parseYAML(textTwo);
     return {
       dataSourceOne: parsed,
       dataSourceTwo: parsedTwo,
-    }
+    };
   } catch (err) {
-    console.log(`Failed: ${codename}`)
+    console.log(`Failed: ${codename}`);
   }
 }
 
 async function main() {
-  const devices = await db('devices').where({ released_on: null })
-  const mapper = async device => {
+  const devices = await db("devices").where({ released_on: null });
+  const mapper = async (device) => {
     const { dataSourceOne, dataSourceTwo } = await deviceInfoAPI(
-      device.codename
-    )
+      device.codename,
+    );
     const releaseDate =
       (dataSourceOne && dataSourceOne.release) ||
-      (dataSourceTwo && dataSourceTwo.release)
+      (dataSourceTwo && dataSourceTwo.release);
 
-    if (!releaseDate) return
+    if (!releaseDate) return;
 
-    const dateData = getReleasedOn(releaseDate)
+    const dateData = getReleasedOn(releaseDate);
 
     if (dateData) {
-      const _date = dateData
-      await db('devices')
+      const _date = dateData;
+      await db("devices")
         .update({
           released_on: _date,
         })
         .where({
           id: device.id,
-        })
+        });
     }
-  }
+  };
 
-  await conch(devices, mapper, { limit: 20 })
+  await conch(devices, mapper, { limit: 20 });
 }
 
 if (fileURLToPath(import.meta.url) === process.argv[1])
-  main().then(() => process.exit(0))
+  main().then(() => process.exit(0));
